c00a2e54a108088341ead0ceb865bd22a8b4fb7d
Alvin Liedén <alvin.lieden@gmail.com>
2026-03-30 22:03:55 +0200

    namnändring

---
+++ lib/include/network_data.h
@@ -0,0 +1,39 @@
+#ifndef NETWORK_DATA
+#define NETWORK_DATA
+
+#define MAX_PLAYERS 6
+
+// BARA RÖRELSER OCH ROLLER IMPLEMENTERADE
+// MER SKA LÄGGAS TILL
+
+typedef struct { // Info som användaren klickar in
+    int player_id;
+    int up;
+    int down;
+    int left;
+    int right;
+} clientInput; 
+
+typedef struct { // Info vart alla är
+    int active;
+    int player_id;
+    int x;
+    int y;
+
+    int isAlive;
+    int isImpostor;
+    int isDoingTask;
+} playerState;
+
+typedef enum{
+    gameLobby,
+    gameRunning,
+    gameMeeting
+} gamePhase;
+
+typedef struct {
+    playerState players[MAX_PLAYERS];
+    gamePhase phase;
+} gameState;
+
+#endif
