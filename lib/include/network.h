1c87d4b6109bdbe83e8d32005c35102aac86ba9e
Alvin Liedén <alvin.lieden@gmail.com>
2026-03-30 13:52:03 +0200

    basic network.h

---
+++ lib/include/network.h
@@ -0,0 +1,39 @@
+#ifndef NETWORK
+#define NETWORK
+
+#define MAX_PLAYERS 6
+
+// BARA RÖRELSER OCH ROLLER IMPLEMENTERADE
+// MER SKA LÄGGAS TILL
+
+typedef struct { // Info som användaren klickar in
+    int player_id;
+    int up;
+    int down;
+    int left;
+    int right;
+} clientInput; 
+
+typedef struct { // Info vart alla är
+    int active;
+    int player_id;
+    int x;
+    int y;
+
+    int isAlive;
+    int isImpostor;
+    int isDoing_task;
+} playerState;
+
+typedef enum{
+    gameLobby,
+    gameRunning,
+    gameMeeting
+} gamePhase;
+
+typedef struct {
+    playerState players[MAX_PLAYERS];
+    gamePhase phase;
+} gameState;
+
+#endif
